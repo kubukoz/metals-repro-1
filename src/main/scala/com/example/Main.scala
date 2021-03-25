df2688a8c6e32458d61f52be172f73ed2812c968
Jakub Kozłowski <kubukoz@gmail.com>
2021-03-25 18:09:34 +0100

    Minimize more

--- src/main/scala/com/example/Main.scala
+++ src/main/scala/com/example/Main.scala
@@ -13,19 +13,13 @@ object demo0 {
   def onEmptyOrNonEmpty[F[_]: MonadThrow, A, B](
     stream: Stream[F, A]
   )(
-    onNonEmpty: Stream[F, A] => F[B]
-  )(
     onEmpty: F[B]
   )(
     implicit SC: fs2.Compiler[F, F]
   ): F[B] = stream
     .pull
     .peek1
-    .flatMap {
-      _.traverse_ { case (h, t) =>
-        Pull.output1(onNonEmpty((Stream.emit(h) ++ t)))
-      }
-    }
+    .void
     .stream
     .compile
     .last
